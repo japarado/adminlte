f3ce1e0fd0ae739f25f0d740fa8ed04153e6774c
pam <justin.parado.personal@gmail.com>
2020-11-27 17:10:17 +0800

    Index page and handler for contacts

---
+++ resources/views/contacts/index.blade.php
@@ -0,0 +1,52 @@
+@extends('adminlte::page')
+
+@section('title', 'Vouchers')
+
+@section('content_header')
+	<h1>Vouchers</h1>
+@stop
+
+@section('content')
+
+	<div class="row">
+		<div class="col-12">
+			<div class="card">
+				<div class="card-header">
+					<h3 class="card-title">Vouchers</h3>
+				</div>
+				<div class="card-body">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="table-responsive">
+								<table id="js-card-table"
+									   class="table table-bordered table-striped">
+									<thead>
+										<tr role="row">
+											<th>ID</th>
+											<th>First Name</th>
+											<th>Last Name</th>
+											<th>Phone Number</th>
+											<th>Email</th>
+										</tr>
+									</thead>
+									<tbody>
+										@foreach($contacts as $contact)
+											<tr>
+												<td>{{ $contact->id }}</td>
+												<td>{{ $contact->first_name }}</td>
+												<td>{{ $contact->last_name }}</td>
+												<td>{{ $contact->phone_number }}</td>
+												<td>{{ $contact->email ?: "N/A" }}</td>
+											</tr>
+										@endforeach
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+					{{ $contacts->links() }}
+				</div>
+			</div>
+		</div>
+	</div>
+@stop
