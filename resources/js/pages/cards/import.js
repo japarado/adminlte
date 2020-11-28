df36d6be29c1e77753772606934820fa00d64b7f
pam <justin.parado.personal@gmail.com>
2020-11-28 13:05:14 +0800

    Removed event-based table updates

--- resources/js/pages/cards/import.js
+++ resources/js/pages/cards/import.js
@@ -6,12 +6,22 @@ import Swal from "sweetalert2";
 import {cardIndex, parseCardCsvData, cardImport, cardAssignBrands} from "../../services/index";
 import {forEach} from "lodash";
 
-document.getElementById("js-import-submit").addEventListener("click", (e) => {
+initializeReviewTable();
+
+document.getElementById("js-import-submit").addEventListener("click", (e) => 
+{
 	e.preventDefault();
 	domClearCardsErrors();
-	domClearParsedData();
+
+	initializeReviewTable();
+	REVIEW_TABLE_DATA = [];
+	initializeReviewTable();
+
 	handleImport();
 });
+
+let REVIEW_TABLE;
+let REVIEW_TABLE_DATA = [];
 
 async function handleImport()
 {
@@ -19,7 +29,8 @@ async function handleImport()
 	try 
 	{
 		const response = await parseCardCsvData(cards);
-		domSetParsedData(JSON.stringify(response.data.cards));
+		REVIEW_TABLE_DATA = response.data.cards;
+		initializeReviewTable();
 		Swal.fire({
 			title: "Merging Success",
 			text: "Please refer to the table in Step 2 to review your imported data",
@@ -43,62 +54,10 @@ async function handleImport()
 	}
 }
 
-function triggerErrorModal()
-{
-	const elementErrorList = document.getElementById("js-import-errors").cloneNode(true);
-	Swal.fire({
-		title: "Given data is invalid",
-		html: elementErrorList,
-		icon: "error"
-	});
-}
-
-function domSetFilesErrors(errors)
-{
-	console.log(errors);
-	const domErrorList = document.getElementById("js-import-errors");
-	errors.forEach((errorMessage) => {
-		const error = document.createElement("li");
-		error.className = "list-group-item list-group-item-danger";
-		error.innerText = errorMessage;
-		domErrorList.appendChild(error);
-	});
-}
-
-function domGetParsedData()
-{
-	return JSON.parse(document.getElementById("js-parsed-results-hidden").value);
-}
-
-function domSetParsedData(mergedData)
-{
-	const field = document.getElementById("js-parsed-results-hidden");
-	field.value = mergedData;
-	field.dispatchEvent(new Event("change"));
-}
-
-function domClearParsedData()
-{
-	const field = document.getElementById("js-parsed-results-hidden");
-	field.value = JSON.stringify([]);
-	field.dispatchEvent(new Event("change"));
-}
-
-// Cleanup functions
-function domClearCardsErrors()
-{
-	document.getElementById("js-import-errors").innerHTML = "";
-}
-
-/******************************* END MERGE **********************/
-
-/******************************* REVIEW DATA **********************/
-document.getElementById("js-parsed-results-hidden").addEventListener("change", handleUpdateMergeData);
-function handleUpdateMergeData(e)
+function initializeReviewTable()
 {
 	const mergeTableContainer = document.getElementById("js-import-review-table");
 	mergeTableContainer.innerHTML = "";
-	const data = e.target.value;
 
 	const colHeaders = [
 		"Abbott Code",
@@ -118,32 +77,67 @@ function handleUpdateMergeData(e)
 		{data: "brand", editor: false},
 	];
 
-	new HandsonTable(mergeTableContainer, {
-		data: JSON.parse(data),
+	REVIEW_TABLE = new HandsonTable(mergeTableContainer, {
+		data: REVIEW_TABLE_DATA,
 		stretchH: "all",
 		rowHeaders: true,
 		colHeaders: colHeaders,
 		columns: columns,
 		height: "20rem",
 		licenseKey: "non-commercial-and-evaluation",
-		afterChange: (changes, source) => {console.table(changes); console.log(source);}
+		afterChange: (changes, source) => 
+		{
+			console.table(changes); console.log(source);
+		}
 	});
 }
-/******************************* END REVIEW DATA **********************/
+
+
+function triggerErrorModal()
+{
+	const elementErrorList = document.getElementById("js-import-errors").cloneNode(true);
+	Swal.fire({
+		title: "Given data is invalid",
+		html: elementErrorList,
+		icon: "error"
+	});
+}
+
+function domSetFilesErrors(errors)
+{
+	console.log(errors);
+	const domErrorList = document.getElementById("js-import-errors");
+	errors.forEach((errorMessage) => 
+	{
+		const error = document.createElement("li");
+		error.className = "list-group-item list-group-item-danger";
+		error.innerText = errorMessage;
+		domErrorList.appendChild(error);
+	});
+}
+
+// Cleanup functions
+function domClearCardsErrors()
+{
+	document.getElementById("js-import-errors").innerHTML = "";
+}
 
 document.getElementById("js-auto-assign-brands").addEventListener("click", handleAssignBrands);
-
 async function handleAssignBrands(e) 
 {
-	const cards = domGetParsedData();
+	const cards = REVIEW_TABLE_DATA;
 	if(e.target.checked)
 	{
 		const response = await cardAssignBrands(cards);
-		domSetParsedData(JSON.stringify(response.data.cards));
+		REVIEW_TABLE_DATA = response.data.cards;
 	}
 	else
 	{
-		const cardsWithRemovedBrands = cards.map((card) => {card.brand = null; return card;});
-		domSetParsedData(JSON.stringify(cardsWithRemovedBrands));
+		const cardsWithRemovedBrands = cards.map((card) => 
+		{
+			card.brand = null; return card;
+		});
+		REVIEW_TABLE_DATA = cardsWithRemovedBrands;
 	}
+	initializeReviewTable();
 }
