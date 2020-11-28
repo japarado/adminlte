51c9fe7a89d1ec8adf4473cc5bf59a81a93a8598
pam <justin.parado.personal@gmail.com>
2020-11-28 10:06:43 +0800

    Registered and created empty card observer class

---
+++ app/Observers/CardObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Card;
+
+class CardObserver
+{
+    /**
+     * Handle the Card "created" event.
+     *
+     * @param  \App\Models\Card  $card
+     * @return void
+     */
+    public function created(Card $card)
+    {
+        //
+    }
+
+    /**
+     * Handle the Card "updated" event.
+     *
+     * @param  \App\Models\Card  $card
+     * @return void
+     */
+    public function updated(Card $card)
+    {
+        //
+    }
+
+    /**
+     * Handle the Card "deleted" event.
+     *
+     * @param  \App\Models\Card  $card
+     * @return void
+     */
+    public function deleted(Card $card)
+    {
+        //
+    }
+
+    /**
+     * Handle the Card "restored" event.
+     *
+     * @param  \App\Models\Card  $card
+     * @return void
+     */
+    public function restored(Card $card)
+    {
+        //
+    }
+
+    /**
+     * Handle the Card "force deleted" event.
+     *
+     * @param  \App\Models\Card  $card
+     * @return void
+     */
+    public function forceDeleted(Card $card)
+    {
+        //
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Card;
+use App\Observers\CardObserver;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 		Paginator::useBootstrap();
+		Card::observe(CardObserver::class);
     }
 }
