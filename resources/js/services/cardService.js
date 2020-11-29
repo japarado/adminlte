e44f33a398850fabf6228e9e9ac4f01af839c4a6
pam <justin.parado.personal@gmail.com>
2020-11-30 00:59:00 +0800

    Code formatting

--- resources/js/services/cardService.js
+++ resources/js/services/cardService.js
@@ -7,14 +7,15 @@ async function index()
 	return await axios.get(prefix);
 }
 
-async function parseCsvData(cards) {
+async function parseCsvData(cards) 
+{
 	const formData = new FormData();
-	formData.append("cards", cards)
+	formData.append("cards", cards);
 	return await axios.post(
 		`${prefix}/parse-csv-data`,
 		formData,
 		{
-			headers: {'Content-Type': "multipart/form-data"}
+			headers: {"Content-Type": "multipart/form-data"}
 		}
 	);
 }
