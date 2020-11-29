334648f786bb9ccfc633cad0680f745c8e3b20b8
pam <justin.parado.personal@gmail.com>
2020-11-30 00:57:56 +0800

    Form validation rules for import cards request

--- app/Http/Requests/ImportCards.php
+++ app/Http/Requests/ImportCards.php
@@ -24,7 +24,25 @@ class ImportCards extends FormRequest
     public function rules()
     {
         return [
-            //
+            'cards.*.abbott_code' => 'required|max:4|alpha_num|exists:abbott_code,code',
+            'cards.*.card_code' => 'required|min:14|alpha_num|unique:card,code',
+
+            'cards.*.first_name' => 'required_with_all:cards.*.last_name,cards.*.phone_number|max:255',
+            'cards.*.last_name' => 'required_with_all:cards.*.first_name,cards.*.phone_number|max:255',
+            'cards.*.phone_number' => 'required_with_all:cards.*first_name,cards.*.last_name',
+            'cards.*.email' => 'nullable|email',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            '0' => 'Abbott code',
+            '1' => 'Card code',
+            '2' => 'Last name',
+            '3' => 'First name',
+            '4' => 'Mobile number',
+            '5' => 'Email'
         ];
     }
 }
