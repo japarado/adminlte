9f7ee3a5addac87f724c63c17cbdbff936391c51
pam <justin.parado.personal@gmail.com>
2020-11-30 11:51:24 +0800

    Remove route group from REST endpoints

--- routes/api.php
+++ routes/api.php
@@ -13,18 +13,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
 
-Route::middleware([])->group(function () {
-    Route::prefix('cards')->group(function () {
-        Route::post('assign-brands', [CardControllerJson::class, 'assignBrands'])->name('api.cards.assign-brands');
-        Route::post('parse-csv-data', [CardControllerJson::class, 'parseCsvData'])->name('api.cards.parse-csv-data');
-        Route::post('import', [CardControllerJson::class, "import"])->name('api.cards.import');
-    });
-
-    Route::resource('cards', CardControllerJson::class, ['as' => 'api.cards'])->parameter('cards', 'id');
+Route::prefix('cards')->group(function () {
+	Route::post('assign-brands', [CardControllerJson::class, 'assignBrands'])->name('api.cards.assign-brands');
+	Route::post('parse-csv-data', [CardControllerJson::class, 'parseCsvData'])->name('api.cards.parse-csv-data');
+	Route::post('import', [CardControllerJson::class, "import"])->name('api.cards.import');
 });
+
+Route::resource('cards', CardControllerJson::class, ['as' => 'api.cards'])->parameter('cards', 'id');
