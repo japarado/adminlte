f476547f4b2c1b1d4016b203a9c278c139e602c2
pam <justin.parado.personal@gmail.com>
2020-11-30 18:57:10 +0800

    Formatting

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -9,39 +9,39 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Card extends Model
 {
     use HasFactory;
-	use SoftDeletes;
+    use SoftDeletes;
 
-	protected $table = "card";
-	
-	protected $fillable = ["code", "batch_id", "abbott_code_id", 'brand_id'];
+    protected $table = "card";
+    
+    protected $fillable = ["code", "batch_id", "abbott_code_id", 'brand_id'];
 
-	public function contact()
-	{
-		return $this->morphOne(Contact::class, 'contactable');
-	}
+    public function contact()
+    {
+        return $this->morphOne(Contact::class, 'contactable');
+    }
 
-	public function cardImage()
-	{
-		return $this->hasOne(CardImage::class);
-	}
+    public function cardImage()
+    {
+        return $this->hasOne(CardImage::class);
+    }
 
-	public function batch()
-	{
-		return $this->belongsTo(Batch::class);
-	}
+    public function batch()
+    {
+        return $this->belongsTo(Batch::class);
+    }
 
-	public function abbottCode()
-	{
-		return $this->belongsTo(AbbottCode::class);
-	}
+    public function abbottCode()
+    {
+        return $this->belongsTo(AbbottCode::class);
+    }
 
-	public function smsLog()
-	{
-		return $this->morphOne(SmsLog::class, 'loggable');
-	}
+    public function smsLog()
+    {
+        return $this->morphOne(SmsLog::class, 'loggable');
+    }
 
-	public function getCodePrefixAttribute()
-	{
-		return substr($this->code, 0, 4);
-	}
+    public function getCodePrefixAttribute()
+    {
+        return substr($this->code, 0, 4);
+    }
 }
