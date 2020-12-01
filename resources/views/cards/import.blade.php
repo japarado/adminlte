9ed868b80a3ba15a5c6e92503e3b4943ad946ae0
pam <justin.parado.personal@gmail.com>
2020-12-01 11:40:55 +0800

    Card import page: put review table in its own row

--- resources/views/cards/import.blade.php
+++ resources/views/cards/import.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Merge Cards')
 
 @section('content_header')
-    <h1>Import Cards</h1>
+	<h1>Import Cards</h1>
 @stop
 
 @section('content')
@@ -16,7 +16,7 @@
 	</div>
 
 	<div class="row">
-		<div class="col-3">
+		<div class="col-12">
 			<div class="card card-primary">
 				<div class="card-header">
 					<h3 class="card-title">1. Import </h3>
@@ -39,7 +39,9 @@
 				</form>
 			</div>
 		</div>
-		<div class="col-9">
+	</div>
+	<div class="row">
+		<div class="col-12">
 			<div class="card card-info">
 				<div class="card-header">
 					<h3 class="card-title">2. Review and Validate Merged Data</h3>
